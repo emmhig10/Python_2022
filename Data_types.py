28b1e2e458b180b0af12de834840a6813fd7839e
emmhig10 <emmhig10@kingshigh.school.nz>
2022-02-25 12:25:20 +1300

    data types

---
+++ Data_types.py
@@ -0,0 +1,2 @@
+print("Finally I felt the calming breeze \n stepping out to watch the final scene \n after all, it's you, my pride, and me \n I can't speak, whatever I can speak you see")
+print("Hulk Like Real Fire.")
