3f35632aa3069f31e516d944dbe628ba6a8f7643
emmhig10 <emmhig10@kingshigh.school.nz>
2022-02-28 13:24:46 +1300

    4.4 qu 9

--- Data_types.py
+++ Data_types.py
@@ -1,2 +1,9 @@
 print("Finally I felt the calming breeze \n stepping out to watch the final scene \n after all, it's you, my pride, and me \n I can't speak, whatever I can speak you see")
 print("Hulk Like Real Fire.")
+print(" A string is a secuence of characters, visible or unvisible \n it is also always in quatations")
+print("an integer is a whole number and does not need quatations")
+print("A float is a number that is a number but is not an integer,like 13.2")
+print(543/3) 
+print(int(543/3))
+print(547%3)
+5
